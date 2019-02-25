4db24b973212aafc9d8aa0bc6fb35a4b5b23b55a
VictorAung <VictorAung2021@u.northwestern.edu>
2019-02-24 21:33:13 -0600

    it works I guess?

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -2,6 +2,7 @@ from game_master import GameMaster
 from read import *
 from util import *
 
+
 class TowerOfHanoiGame(GameMaster):
 
     def __init__(self):
@@ -33,8 +34,23 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             A Tuple of Tuples that represent the game state
         """
-        ### student code goes here
-        pass
+        p1 = []
+        p2 = []
+        p3 = []
+        for fact in self.kb.facts:
+            if fact.statement.predicate == 'on':
+                disk = fact.statement.terms[0].term.element
+                num = int(disk[-1:])
+                if fact.statement.terms[1].term.element == 'peg1':
+                    p1.append(num)
+                if fact.statement.terms[1].term.element == 'peg2':
+                    p2.append(num)
+                if fact.statement.terms[1].term.element == 'peg3':
+                    p3.append(num)
+        p1.sort()
+        p2.sort()
+        p3.sort()
+        return tuple(p1), tuple(p2), tuple(p3)
 
     def makeMove(self, movable_statement):
         """
@@ -52,8 +68,42 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             None
         """
-        ### Student code goes here
-        pass
+
+        if self.isMovableLegal(movable_statement):
+            movableQuery = self.produceMovableQuery()
+            matchCheck = match(movable_statement, movableQuery.statement)
+            if matchCheck:
+                disc1 = matchCheck.bindings[0].constant
+                pInitial = matchCheck.bindings[1].constant
+                pTarget = matchCheck.bindings[2].constant
+                self.kb.kb_retract(parse_input('fact: (top ' + disc1.element + ' ' + pInitial.element + ')'))
+                self.kb.kb_retract(parse_input('fact: (on ' + disc1.element + ' ' + pInitial.element + ')'))
+
+                aboveCheck = self.kb.kb_ask(parse_input('fact: (above ' + disc1.element + ' ?x)'))
+                if aboveCheck:
+                    oldBottom = aboveCheck[0].bindings_dict['?x']
+                    self.kb.kb_retract(parse_input('fact: (above ' + disc1.element + ' ' + oldBottom + ')'))
+                    self.kb.kb_assert(parse_input('fact: (top ' + oldBottom + ' ' + pInitial.element + ')'))
+
+                else:
+                    self.kb.kb_assert(parse_input('fact: (empty ' + pInitial.element + ')'))
+
+                emptyCheck = self.kb.kb_ask(parse_input('fact: (empty ' + pTarget.element + ')'))
+                if emptyCheck:
+                    self.kb.kb_retract(parse_input('fact: (empty ' + pTarget.element + ')'))
+                    self.kb.kb_assert(parse_input('fact: (on ' + disc1.element + ' ' + pTarget.element + ')'))
+                    self.kb.kb_assert(parse_input('fact: (top ' + disc1.element + ' ' + pTarget.element + ')'))
+
+                else:
+                    currTop = self.kb.kb_ask(parse_input('fact: (top ?x ' + pTarget.element + ')'))
+                    oldTop = currTop[0].bindings_dict['?x']
+                    self.kb.kb_retract(parse_input('fact: (top ' + oldTop + ' ' + pTarget.element + ')'))
+                    self.kb.kb_assert(parse_input('fact: (above ' + disc1.element + ' ' + oldTop))
+                    self.kb.kb_assert(parse_input('fact: (on ' + disc1.element + ' ' + pTarget.element + ')'))
+                    self.kb.kb_assert(parse_input('fact: (top ' + disc1.element + ' ' + pTarget.element))
+
+        else:
+            pass
 
     def reverseMove(self, movable_statement):
         """
@@ -70,8 +120,8 @@ class TowerOfHanoiGame(GameMaster):
         newList = [pred, sl[0], sl[2], sl[1]]
         self.makeMove(Statement(newList))
 
-class Puzzle8Game(GameMaster):
 
+class Puzzle8Game(GameMaster):
     def __init__(self):
         super().__init__()
 
@@ -100,7 +150,25 @@ class Puzzle8Game(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### Student code goes here
-        pass
+        row1 = [0, 0, 0]
+        row2 = [0, 0, 0]
+        row3 = [0, 0, 0]
+        for fact in self.kb.facts:
+            if fact.statement.predicate == 'pos':
+                ele = fact.statement.terms[0].term.element
+                num = ele[-1:]
+                if num != 'y':
+                    num = int(num)
+                else:
+                    num = -1
+                col = int(fact.statement.terms[1].term.element[-1:])
+                if fact.statement.terms[2].term.element == 'pos1':
+                    row1[col - 1] = num
+                if fact.statement.terms[2].term.element == 'pos2':
+                    row2[col - 1] = num
+                if fact.statement.terms[2].term.element == 'pos3':
+                    row3[col - 1] = num
+        return tuple(row1), tuple(row2), tuple(row3)
 
     def makeMove(self, movable_statement):
         """
@@ -119,7 +187,20 @@ class Puzzle8Game(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        if self.isMovableLegal(movable_statement):
+            ele = movable_statement.terms[0].term.element
+            col = movable_statement.terms[1].term.element
+            row = movable_statement.terms[2].term.element
+            col1 = movable_statement.terms[3].term.element
+            row1 = movable_statement.terms[4].term.element
+            oldFact = parse_input('fact: (pos ' + ele + ' ' + col + ' ' + row + ')')
+            newFact = parse_input('fact: (pos ' + ele + ' ' + col1 + ' ' + row1 + ')')
+            oldFact1 = parse_input('fact: (pos empty ' + col1 + ' ' + row1 + ')')
+            newFact1 = parse_input('fact: (pos empty ' + col + ' ' + row + ')')
+            self.kb.kb_retract(oldFact)
+            self.kb.kb_retract(oldFact1)
+            self.kb.kb_assert(newFact)
+            self.kb.kb_assert(newFact1)
 
     def reverseMove(self, movable_statement):
         """
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -1,5 +1,7 @@
 
+from collections import deque
 from solver import *
+
 
 class SolverDFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
@@ -19,12 +21,38 @@ class SolverDFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        return True
+
+
+
+        # save current state
+        currState = self.currentState
+
+        # if there are next moves, we should expand the node and mark down parents and children
+        for mov in self.gm.getMovables():
+            self.gm.makeMove(mov)
+            nextState = GameState(self.gm.getGameState(), self.currentState.depth + 1, mov)
+            currState.children.append(nextState)
+            nextState.parent = currState
+            self.gm.reverseMove(mov)
+
+        # check child if it has not been visited
+        for child in currState.children:
+            if child not in self.visited:
+                self.currentState = child
+                self.visited[child] = True
+                self.gm.makeMove(child.requiredMovable)
+                break
+
+        if self.victoryCondition == self.currentState.state:
+            return True
+        return False
 
 
 class SolverBFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+        self.queue = deque()
+        self.counter = 0
 
     def solveOneStep(self):
         """
@@ -40,4 +68,36 @@ class SolverBFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        return True
+        #queue up state
+        for move in self.gm.getMovables():
+            self.gm.makeMove(move)
+            newState = GameState(self.gm.getGameState(), self.currentState.depth + 1, move)
+            newState.parent = self.currentState
+            self.gm.reverseMove(move)
+            self.currentState.children.append(newState)
+            self.queue.append(newState)
+
+        #go to root
+        while self.currentState.parent:
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+
+        #change counter
+        while self.queue[self.counter] in self.visited:
+            self.counter += 1
+
+        newList = list()
+        while self.queue[self.counter].parent:
+            newList.append(self.queue[self.counter].requiredMovable)
+            self.queue[self.counter] = self.queue[self.counter].parent
+
+        while len(newList) != 0:
+            self.gm.makeMove(newList.pop())
+            for child in self.currentState.children:
+                if child.state == self.gm.getGameState():
+                    self.visited[child] = True
+                    self.currentState = child
+
+        if self.currentState.state == self.victoryCondition:
+            return True
+        return False
